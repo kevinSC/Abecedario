1d03083ac3854df14ae997f27956401435c8f9f3
KevinSC <kevineitor22@gmail.com>
2016-02-20 17:28:59 -0500

    se cambio la funcion anterior por cuttinStr y se soluciono el problema inical propuesto en la guia

--- abecedario.py
+++ abecedario.py
@@ -1,14 +1,15 @@
-def abcGen(letter, reverse=False):
-	c = 0
-	letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-	if reverse:
-		letters = letters[-1::-1]
-	for x in range(len(letters)):
-		if letter == letters[x]:
-			c = x
-			break
-	print(letters[:c + 1])
+def cuttingStr(strin, number):
+	return strin[:number + 1]
 
-abcGen('b', True)
-
-input('ingrese a continuacion la letra a la hasta la cual desea generar la piramide\n:')
+letters = 'abcdefghijklmnopqrstuvwxyz'
+letter = input('ingresa la letra limite\n:').lower()
+letters = letters[:letters.find(letter) + 1]
+spaceX = list(range(len(letters)))
+spaceY = spaceX[:]
+indice = len(spaceY) - 1
+spaceY.insert(indice, spaceX[indice - 1])
+spaceY = spaceY[:-1]
+space0 = list(range((2 * indice) - 1, -1, -2))
+space0.append(0)
+for x, y, o in zip(spaceX, spaceY, space0):
+	print(cuttingStr(letters, x) + o * " " + cuttingStr(letters, y)[-1::-1])
