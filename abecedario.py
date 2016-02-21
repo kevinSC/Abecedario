932b0dd51f67a6085ce0e6b8da87b93d8525d57c
KevinSC <kevineitor22@gmail.com>
2016-02-20 19:03:20 -0500

    comit para hacer checkout

--- abecedario.py
+++ abecedario.py
@@ -1,15 +1,5 @@
-def cuttingStr(strin, number):
-	return strin[:number + 1]
-
-letters = 'abcdefghijklmnopqrstuvwxyz'
-letter = input('ingresa la letra limite\n:').lower()
-letters = letters[:letters.find(letter) + 1]
-spaceX = list(range(len(letters)))
-spaceY = spaceX[:]
-indice = len(spaceY) - 1
-spaceY.insert(indice, spaceX[indice - 1])
-spaceY = spaceY[:-1]
-space0 = list(range((2 * indice) - 1, -1, -2))
-space0.append(0)
-for x, y, o in zip(spaceX, spaceY, space0):
-	print(cuttingStr(letters, x) + o * " " + cuttingStr(letters, y)[-1::-1])
+xaxis = int(input('ingrese el numero de caracteres que desea ver horizontalmente:\n'))
+yaxis = int(input('ingrese el numero de caracteres que desea ver verticalmente:\n'))
+letter = input('ingrese la letra limite de la piramide:\n')
+for x in xrange(1,10):
+	pass
