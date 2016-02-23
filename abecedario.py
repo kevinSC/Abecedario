a1c3c366b3a3f3aa24bd4cb9669167aeb353b78e
KevinSC <kevineitor22@gmail.com>
2016-02-22 19:46:45 -0500

    se completo el proyecto en python exitosamente se cambiaron las funciones y se dejo ciclos puros con condicionales por que creo que es mas practico y el problema no amerita complicarse inecesariamente

--- abecedario.py
+++ abecedario.py
@@ -1,5 +1,26 @@
-xaxis = int(input('ingrese el numero de caracteres que desea ver horizontalmente:\n'))
-yaxis = int(input('ingrese el numero de caracteres que desea ver verticalmente:\n'))
-letter = input('ingrese la letra limite de la piramide:\n')
-for x in xrange(1,10):
-	pass
+xaxis = range(int(input('ingrese el numero de caracteres que desea ver horizontalmente:\n')))  # numero de caracteres que se mostraran horizontalmente
+yaxis = range(int(input('ingrese el numero de caracteres que desea ver verticalmente:\n')))  # numero de caracteres que se mostrara verticalmente
+letters = 'abcdefghijklmnopqrstuvwxyz'
+letters = letters[:letters.find(input('ingresa la letra limite:\n').lower()) + 1]  # String que contiene desde la a la letra ingresada
+changin = letters  # variable que guarda copia a letters para luego ser modificado
+lon = len(letters) - 1  # variable que guarda el numero de subindices de letters
+white, rev = 0, False  # white determina el numero de espacios condicionada por rev
+for y in yaxis:
+	limit = 0
+	changin = letters[:white + 1] + (lon - white) * ' '
+	for x in xaxis:
+		if limit == (lon + 1):
+			changin = changin[-1::-1]
+			limit = 1
+		print(changin[limit], end='')
+		limit += 1
+	print('')
+	if rev:
+		white -= 1
+	else:
+		white += 1
+	if white == lon:
+		rev = True
+	if white == 0:
+		rev = False
+	changin = letters
