50b60c2397b80713dcacdd4b3d55d31668f45666
KevinSC <kevineitor22@gmail.com>
2016-02-23 22:01:34 -0500

    experimento fallido con el foreach

--- abecedario.cpp
+++ abecedario.cpp
@@ -1,14 +1,13 @@
 // string::find
 #include <iostream>       // std::cout
 #include <string>         // std::string
-
 using namespace std;
 int main ()
 {
-  string str  = "There are two needles in this haystack with needles.";
-  string str2 ("needle");
-  size_t found = str.find(str2);
-  cout << "first 'needle' found at: " << found % 3 << '\n';
+    string list = "abc";
+    for ( string i: list){
+        std::cout << i  << "hola" << std::endl;
+    }
 
  
   return 0;
